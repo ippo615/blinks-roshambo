ce80be02f233da7c10e65f4ee875f0981e9db56f
Andrew Ippoliti <acippolit@gmail.com>
2020-02-03 01:02:33 -0500

    Modify game style to make it easier to see the score

--- blinks_roshambo/blinks_roshambo.ino
+++ blinks_roshambo/blinks_roshambo.ino
@@ -6,34 +6,45 @@
  * License: MIT
  */
 
-#define N_THINGS 3
+/**
+ * Styles
+ */
+#define N_TEAMS 7
+Color TEAM_COLORS[] = {
+  RED,BLUE,YELLOW,MAGENTA,GREEN,ORANGE,CYAN
+};
+#define BRIGHTNESS_WON 255
+#define BRIGHTNESS_LOST 0
+#define BRIGHTNESS_TIE 32
+#define BRIGHTNESS_LONELY 0
 
-#define VALUE_NOT_READY 0
-#define VALUE_ROCK 1
-#define VALUE_PAPER 2
-#define VALUE_SCISSOR 4
+/**
+ * Definitions for Readability/Maintence
+ */
+enum {
+  ROCK,
+  PAPER,
+  SCISSOR
+};
 
-#define COLOR_ROCK makeColorRGB(255,0,0)
-#define COLOR_PAPER makeColorRGB(0,255,0)
-#define COLOR_SCISSOR makeColorRGB(0,0,255)
-#define COLOR_NONE makeColorRGB(0,0,0)
-#define COLOR_WIN makeColorRGB(0,255,0)
-#define COLOR_LOSE makeColorRGB(255,0,0)
-#define COLOR_TIE makeColorRGB(0,0,255)
+#define N_PIECE_TYPES 3
+byte PIECE_TYPES[] = {ROCK,PAPER,SCISSOR};
+byte values[] = {ROCK,PAPER,SCISSOR};
 
-Color colors[] = {COLOR_ROCK,COLOR_PAPER,COLOR_SCISSOR};
-byte values[] = {VALUE_ROCK,VALUE_PAPER,VALUE_SCISSOR};
-
-#define MODE_INPUT 0
-#define MODE_BATTLE 1
+#define MODE_START 0
+#define MODE_TEAM_SELECT 1
+#define MODE_TEAM_PROPAGATE 2
+#define MODE_PIECE_SELECT 3
+#define MODE_BOARD 4
 
 #define RESULT_FACE_WON 0
 #define RESULT_FACE_LOST 1
 #define RESULT_FACE_TIE 2
 #define RESULT_FACE_LONELY 3
 
-byte selection = 0;
-byte mode = MODE_INPUT;
+/**
+ * Game State
+ */
 byte results[] = {
   RESULT_FACE_LONELY,
   RESULT_FACE_LONELY,
@@ -42,63 +53,119 @@ byte results[] = {
   RESULT_FACE_LONELY,
   RESULT_FACE_LONELY
 };
+byte mode = MODE_TEAM_SELECT;
+byte team_index = 0;
+byte piece_type_index = 0;
 
+
+/**
+ * Initialization
+ */
 void setup() {
   randomize();
-  setValueSentOnAllFaces(VALUE_NOT_READY);
-  selection = getSerialNumberByte(0) % N_THINGS;
+  //setValueSentOnAllFaces(VALUE_NOT_READY);
+  team_index = getSerialNumberByte(0) % N_TEAMS;
 }
 
-void loop() {
-
-  /**
-   * INPUT MODE
-   * The player can single click to change their selection.
-   * All the faces show the color of the current selection.
-   * By long clicking they turn off the color and enter battle mode.
-   */
-  if( mode == MODE_INPUT ){
-    if (buttonSingleClicked()) {
-      selection = selection + 1;
-      if ( selection >= N_THINGS ) {
-        selection = 0; 
-      }
-    }
-    setColor( colors[selection] );
-    setValueSentOnAllFaces( VALUE_NOT_READY );
-    if( buttonLongPressed() ){
-      mode = MODE_BATTLE;
-    }
+/**
+ * Loops for each game mode
+ */
+void loop_mode_start(){
+  // maybe add a delay here so that we dont get noise and can propagate team selection
+  mode = MODE_TEAM_SELECT;
+}
+void loop_mode_team_select(){
+  if (buttonSingleClicked()) {
+    team_index = (team_index + 1) % N_TEAMS;
+  }
+  setColor( TEAM_COLORS[team_index] );
+  if( buttonLongPressed() ){
+    mode = MODE_TEAM_PROPAGATE;
   }
 
-  /**
-   * BATTLE MODE
-   * Players put their Blinks together.
-   * Each face lights up to show which side won or lost.
-   * Red is a loss, green is a win, and blue is a tie.
-   * A long press will "reset" into INPUT MODE and randomize
-   * the starting selection (if the starting selection was
-   * not randomized then players chould cheat by listening to
-   * their opponents clicking... now they can only cheat by
-   * observing the color of the light given off by the blink
-   * which can be adjusted with brightness).
-   */
-  if( mode == MODE_BATTLE ){
-    setValueSentOnAllFaces( values[selection] );
-    FOREACH_FACE(f){
-      byte otherValue = getLastValueReceivedOnFace(f);
-      if( ! isValueReceivedOnFaceExpired(f) ){
-        results[f] = result_lookup( values[selection], otherValue );
-      }else{
-        results[f] = RESULT_FACE_LONELY;
-      }
-      result_draw( results[f], f );
+  /*
+   * // Listening seems to be buggy
+  FOREACH_FACE(f){
+    byte otherValue = getLastValueReceivedOnFace(f);
+    if( ! isValueReceivedOnFaceExpired(f) ){
+      team_index = otherValue;
+      mode = MODE_TEAM_PROPAGATE;
     }
+  }
+  */
+}
+void loop_mode_team_propagate(){
+  //setValueSentOnAllFaces( team_index );
+  mode = MODE_PIECE_SELECT;
+}
+void loop_mode_piece_select(){
+  if (buttonSingleClicked()) {
+    piece_type_index = (piece_type_index + 1) % N_PIECE_TYPES;
+  }
+  if( PIECE_TYPES[piece_type_index] == ROCK ){
+    setColorOnFace( TEAM_COLORS[team_index], 0 );
+    setColorOnFace( TEAM_COLORS[team_index], 1 );
+    setColorOnFace( TEAM_COLORS[team_index], 2 );
+    setColorOnFace( TEAM_COLORS[team_index], 3 );
+    setColorOnFace( TEAM_COLORS[team_index], 4 );
+    setColorOnFace( OFF, 5 );
+  }else
+  if( PIECE_TYPES[piece_type_index] == PAPER ){
+    setColorOnFace( OFF, 0 );
+    setColorOnFace( TEAM_COLORS[team_index], 1 );
+    setColorOnFace( TEAM_COLORS[team_index], 2 );
+    setColorOnFace( OFF, 3 );
+    setColorOnFace( TEAM_COLORS[team_index], 4 );
+    setColorOnFace( TEAM_COLORS[team_index], 5 );
+  }else
+  if( PIECE_TYPES[piece_type_index] == SCISSOR ){
+    setColorOnFace( TEAM_COLORS[team_index], 0 );
+    setColorOnFace( OFF, 1 );
+    setColorOnFace( TEAM_COLORS[team_index], 2 );
+    setColorOnFace( OFF, 3 );
+    setColorOnFace( TEAM_COLORS[team_index], 4 );
+    setColorOnFace( OFF, 5 );
+  }
+  if( buttonLongPressed()){
+    mode = MODE_BOARD;
+  }
+}
+void loop_mode_board(){
+  setValueSentOnAllFaces( values[piece_type_index] );
+  FOREACH_FACE(f){
+    byte otherValue = getLastValueReceivedOnFace(f);
+    if( ! isValueReceivedOnFaceExpired(f) ){
+      results[f] = result_lookup( values[piece_type_index], otherValue );
+    }else{
+      results[f] = RESULT_FACE_LONELY;
+    }
+    result_draw( results[f], f );
+  }
 
-    if( buttonLongPressed() ){
-      mode = MODE_INPUT;
-      selection = random(N_THINGS) % N_THINGS;
-    }
+  if( buttonLongPressed() ){
+    mode = MODE_START;
+  }
+}
+
+/**
+ * Main loop
+ */
+void loop() {
+
+  if( mode == MODE_START ){
+    loop_mode_start();
+  }else
+  if( mode == MODE_TEAM_SELECT ){
+    loop_mode_team_select();
+  }else
+  if( mode == MODE_TEAM_PROPAGATE ){
+    loop_mode_team_propagate();
+  }else
+  if( mode == MODE_PIECE_SELECT ){
+    loop_mode_piece_select();
+  }else
+  if( mode == MODE_BOARD ){
+    loop_mode_board();
   }
 
 }
@@ -108,13 +175,13 @@ void loop() {
  */
 void result_draw( byte result, byte face ){
   if( result == RESULT_FACE_WON ){
-    setColorOnFace( COLOR_WIN, face );
+    setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_WON ), face );
   }else if( result == RESULT_FACE_LOST ){
-    setColorOnFace( COLOR_LOSE, face );
+    setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_LOST ), face );
   }else if( result == RESULT_FACE_TIE ){
-    setColorOnFace( COLOR_TIE, face );
+    setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_TIE ), face );
   }else{
-    setColorOnFace( COLOR_NONE, face );
+    setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_LONELY ), face );
   }
 }
 
@@ -126,27 +193,27 @@ byte result_lookup( byte my_value, byte your_value ){
   if( my_value == your_value ){
     return RESULT_FACE_TIE;
   }
-  if( my_value == VALUE_ROCK ){
-    if( your_value == VALUE_PAPER ){
+  if( my_value == ROCK ){
+    if( your_value == PAPER ){
       return RESULT_FACE_LOST;
     }
-    if( your_value == VALUE_SCISSOR ){
+    if( your_value == SCISSOR ){
       return RESULT_FACE_WON;
     }
   }
-  if( my_value == VALUE_PAPER ){
-    if( your_value == VALUE_SCISSOR ){
+  if( my_value == PAPER ){
+    if( your_value == SCISSOR ){
       return RESULT_FACE_LOST;
     }
-    if( your_value == VALUE_ROCK ){
+    if( your_value == ROCK ){
       return RESULT_FACE_WON;
     }
   }
-  if( my_value == VALUE_SCISSOR ){
-    if( your_value == VALUE_ROCK ){
+  if( my_value == SCISSOR ){
+    if( your_value == ROCK ){
       return RESULT_FACE_LOST;
     }
-    if( your_value == VALUE_PAPER ){
+    if( your_value == PAPER ){
       return RESULT_FACE_WON;
     }
   }
