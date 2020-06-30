87f8092647fbdc5d78a0e9f1c0976226ee690894
Andrew Ippoliti <acippolit@gmail.com>
2020-06-29 22:07:47 -0400

    Completely rewrote, new mechanics (more)
    Never tested on hardware. Only tested in blinks web simulator.

--- blinks_roshambo/blinks_roshambo.ino
+++ blinks_roshambo/blinks_roshambo.ino
@@ -1,372 +1,161 @@
-/**
- * Title: Rock, Paper, Scissors, Blinks! (aka RGB)
- * Author: Andrew Ippoliti
- * Platform: https://blinks.games/
- * Instructions: Read the code??
- * License: MIT
- */
 
-/**
- * Styles
- */
-#define N_TEAMS 4
-Color TEAM_COLORS[] = {
-  RED,BLUE,YELLOW,GREEN
-};
-Color COLOR_SELF_WIN_LOSE = WHITE;
-#define BRIGHTNESS_WON 255
-#define BRIGHTNESS_SELF 128
-#define BRIGHTNESS_LOST 0
-#define BRIGHTNESS_TIE 32
-#define BRIGHTNESS_LONELY 0
+#define ROCK 1
+#define PAPER 2
+#define SCISSORS 3
+#define N_CHOICES 3
 
-byte BITMAP_ROCK[] = {255,255,255,255,255,0};
-byte BITMAP_PAPER[] = {0,255,255,0,255,255};
-byte BITMAP_SCISSOR[] = {255,0,255,0,255,0};
-byte* BITMAPS[] = {
-  (byte*)&BITMAP_ROCK,
-  (byte*)&BITMAP_PAPER,
-  (byte*)&BITMAP_SCISSOR,
-};
+#define CLICK_TIMER_DURATION 2000
+#define IDLE_TIMER_DURATION 2000
 
-/**
- * Definitions for Readability/Maintence
- */
-enum {
-  ROCK,
-  PAPER,
-  SCISSOR
-};
+Timer clickTimer;
+Timer idleTimer;
+int choice;
+int visibleChoice = 0;
+int team = 0;
+int brightness = 0;
 
-#define N_PIECE_TYPES 3
-byte PIECE_TYPES[] = {ROCK,PAPER,SCISSOR};
-byte values[] = {ROCK,PAPER,SCISSOR};
+Color teamColors[] = {RED,GREEN,BLUE};
+#define N_TEAMS 3
 
 enum {
-  MODE_START,
-  MODE_TEAM_SELECT,
-  MODE_TEAM_PROPAGATE,
-  MODE_PIECE_SELECT,
-  MODE_PIECE_SELECT_FEEDBACK,
-  MODE_BOARD,
-  MODE_BOARD_INFO
+    MODE_IDLE,
+    MODE_SHOW_CHOICE,
+    MODE_SHOW_BOARD,
+    MODE_CORE
 };
+int mode = MODE_IDLE;
 
-#define DURATION_FEEDBACK_MS 800
-#define DURATION_PIECE_INFO_MS 2000
-Timer timerFeedbackAnimation;
-
-
-
-#define RESULT_FACE_WON 0
-#define RESULT_FACE_LOST 1
-#define RESULT_FACE_TIE 2
-#define RESULT_FACE_LONELY 3
-
-/**
- * Game State
- */
-byte results[] = {
-  RESULT_FACE_LONELY,
-  RESULT_FACE_LONELY,
-  RESULT_FACE_LONELY,
-  RESULT_FACE_LONELY,
-  RESULT_FACE_LONELY,
-  RESULT_FACE_LONELY
-};
-byte infos[] = {0,0,0,0,0,0};
-byte mode = MODE_TEAM_SELECT;
-byte team_index = 0;
-byte piece_type_index = 0;
-
-/*
-// Another way of "bit manipulation" but due to point/address stuff
-// and a few extra lines of code... the macro bit manipulation seems
-// easier to maintain.
-typedef struct {
-  byte team: 3;
-  byte piece: 2;
-} type_data;
-type_data received_datas[] = {0,0,0,0,0,0};
-type_data sent_data = {
-  team: team_index,
-  piece: piece_type_index
-};
-setValueSentOnAllFaces( (byte*)&sent_data );
-*/
-
-/**
- * Data Communication Format
- * We can send a number between 0 and 64. This means we have 6 bits
- * of information.
- * For pieces we have 4 types: 2 bits (full)
- * For teams we have 4 types: 2 bits (full)
- * For functions we have 3 types: 2 bits (not full)
- */
-#define INFO_BUILD(TEAM,PIECE,FUNCTION) ( (FUNCTION<<4) | (TEAM<<2) | PIECE )
-#define INFO_GET_FUNCTION( INFO ) ((INFO&0b00110000)>>4)
-#define INFO_GET_TEAM( INFO ) ((INFO&0b00001100)>>2)
-#define INFO_GET_PIECE( INFO ) (INFO&0b00000011)
-
-enum {
-  FUNCTION_INFORM,
-  FUNCTION_SET_TEAM,
-  FUNCTION_CRUMPLE
-};
-
-/**
- * Initialization
- */
 void setup() {
-  randomize();
-  team_index = getSerialNumberByte(0) % N_TEAMS;
+  setValueSentOnAllFaces(0);
+  choice = 1+random(N_CHOICES);
+  visibleChoice = 1+random(N_CHOICES);
+  team = random(N_TEAMS);
 }
 
-/**
- * Mode: Start
- * 
- * Does nothing really. Maybe I should add some reset functionality here.
- */
-void loop_mode_start(){
-  mode = MODE_TEAM_SELECT;
-}
-
-/**
- * Mode: Team select
- * 
- * You can single click to cycle through the team colors. Once you have selected
- * the team color - long press to lock it in. It will propagate the selection to
- * any connected Blinks.
- */
-void loop_mode_team_select(){
-  if (buttonSingleClicked()) {
-    team_index = (team_index + 1) % N_TEAMS;
-  }
-
-  byte isActivated = false;
-  FOREACH_FACE(f){
-    if(!isValueReceivedOnFaceExpired(f)){
-      byte info = getLastValueReceivedOnFace(f);
-      if( INFO_GET_FUNCTION(info) == FUNCTION_SET_TEAM ){
-        isActivated = true;
-        team_index = INFO_GET_TEAM(info);
-      }
+void loop() {
+    
+    if( isAlone() ){
+        if( buttonLongPressed() ){
+            team = 0;
+            choice = visibleChoice;
+            mode = MODE_CORE;
+        }
+        if( mode == MODE_SHOW_BOARD ){
+            mode = MODE_IDLE;
+        }
     }
-  }
-  if( buttonLongPressed() ){
-    isActivated = true;
-  }
+    if( !isAlone() ){
+        if( buttonPressed() ){
+            mode = MODE_SHOW_BOARD;
+        }
+    }
 
-  setColor( TEAM_COLORS[team_index] );
+    //if( mode == MODE_SHOW_BOARD ){
+    uint8_t data = ((choice&0b11)<<2) | (team&0b11);
+    setValueSentOnAllFaces(data);
+    
+    if( !isAlone() && (mode != MODE_CORE) ){
 
-  if( isActivated ){
-    mode = MODE_TEAM_PROPAGATE;
-    timerFeedbackAnimation.set(DURATION_FEEDBACK_MS);
-  }
-}
-
-/**
- * Mode: Team Propagate
- * 
- * It will animate itself to show the team that it is and will broadcast it's
- * selection to neighboring Blinks.
- */
-void loop_mode_team_propagate(){
-  if( timerFeedbackAnimation.isExpired() ){
-    mode = MODE_PIECE_SELECT;
-  }else{
-    draw_animate_pulse( TEAM_COLORS[team_index], timerFeedbackAnimation, DURATION_FEEDBACK_MS, 2 );
-    setValueSentOnAllFaces( INFO_BUILD( team_index, values[piece_type_index], FUNCTION_SET_TEAM ) );
-  }
-}
-
-/**
- * Mode: Piece Select
- * 
- * Single click to choose between Rock/Paper/Scissor/Crumple.
- * Long click to lock selection.
- */
-void loop_mode_piece_select(){
-  if (buttonSingleClicked()) {
-    piece_type_index = (piece_type_index + 1) % N_PIECE_TYPES;
-  }
-  draw_bitmap( BITMAPS[PIECE_TYPES[piece_type_index]], TEAM_COLORS[team_index], 255 );
-
-  if( buttonLongPressed() ){
-    mode = MODE_PIECE_SELECT_FEEDBACK;
-    timerFeedbackAnimation.set(DURATION_FEEDBACK_MS);
-  }
-}
-
-/**
- * Mode: Piece Select Feedback
- * 
- * Animates the user's piece selection before entering board mode.
- */
-void loop_mode_piece_select_feedback(){
-  if( timerFeedbackAnimation.isExpired() ){
-    mode = MODE_BOARD;
-  }else{
-    byte piece = values[piece_type_index];
-    draw_animate_pulse_bitmap( BITMAPS[piece], TEAM_COLORS[team_index], timerFeedbackAnimation, DURATION_FEEDBACK_MS, 2 );
-  }
-}
-
-/**
- * Mode: Board Mode
- * 
- * Shows winner/loser tie when placed next to another Blink.
- * On single click - animates the piece to show if it is rock/paper/scissor.
- * On long press - resets this Blink for a new round.
- */
-void loop_mode_board(){
-  setValueSentOnAllFaces( INFO_BUILD( team_index, values[piece_type_index], FUNCTION_INFORM ) );
-  FOREACH_FACE(f){
-    byte info = getLastValueReceivedOnFace(f);
-    if( ! isValueReceivedOnFaceExpired(f) ){
-      infos[f] = info;
-      results[f] = result_lookup( values[piece_type_index], values[INFO_GET_PIECE(info)] );
-      result_draw( results[f], INFO_GET_TEAM(info), team_index, f );
-    }else{
-      setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_LONELY ), f );
+        FOREACH_FACE(f){
+            
+            if( mode == MODE_IDLE || mode == MODE_SHOW_CHOICE ){
+                if( isValueReceivedOnFaceExpired(f) ){
+                    setColorOnFace( teamColors[team], f );
+                    continue;
+                }else{
+                    setColorOnFace( WHITE, f );
+                    continue;
+                }
+            }
+            
+            if( isValueReceivedOnFaceExpired(f ) ){
+                if( mode == MODE_SHOW_BOARD ){
+                    setColorOnFace(OFF, f);
+                    continue;
+                }
+            }
+            uint8_t recieved = getLastValueReceivedOnFace(f);
+            uint8_t rTeam = (recieved & 0b0011);
+            uint8_t rChoice = (recieved & 0b1100)>>2;
+            
+            // ties
+            if( (rTeam == team) || (rChoice == choice) ){
+                setColorOnFace(dim(WHITE,255),f);
+                continue;
+            }
+            
+            // LOSING CONDITIONS
+            if(
+                ((rChoice == ROCK) && (choice == SCISSORS)) ||
+                ((rChoice == PAPER) && (choice == ROCK)) ||
+                ((rChoice == SCISSORS) && (choice == PAPER))
+            ){
+                setColorOnFace( dim(teamColors[rTeam],255), f );
+                continue;
+            }
+            // Winning conditions
+            if(
+                ((choice == ROCK) && (rChoice == SCISSORS)) ||
+                ((choice == PAPER) && (rChoice == ROCK)) ||
+                ((choice == SCISSORS) && (rChoice == PAPER))
+            ){
+                setColorOnFace( dim(teamColors[team],255), f );
+                continue;
+            }
+            
+        }
+        return;
     }
     
-  }
+    if( mode == MODE_IDLE ){
+        if( isAlone() ){
+          if( buttonPressed() ){
+            clickTimer.set(CLICK_TIMER_DURATION);      
+            choice = visibleChoice;
+            mode = MODE_SHOW_CHOICE;
+        }
+      }
+    }
 
-  if( buttonLongPressed() ){
-    mode = MODE_START;
-  }
-
-  if( buttonSingleClicked() ){
-    mode = MODE_BOARD_INFO;
-    timerFeedbackAnimation.set(DURATION_PIECE_INFO_MS);
-  }
-}
-
-/**
- * Mode: Board Info
- * 
- * Shows the piece (rock/paper/scissor) that this Blink is.
- * Then returns to board mode.
- */
-void loop_mode_board_info(){
-  if( timerFeedbackAnimation.isExpired() ){
-    mode = MODE_BOARD;
-  }else{
-    byte piece = values[piece_type_index];
-    draw_animate_pulse_bitmap( BITMAPS[piece], TEAM_COLORS[team_index], timerFeedbackAnimation, DURATION_PIECE_INFO_MS, 1 );
-  }
-}
-
-/**
- * Main loop
- */
-void loop() {
-
-  if( mode == MODE_START ){
-    loop_mode_start();
-  }else
-  if( mode == MODE_TEAM_SELECT ){
-    loop_mode_team_select();
-  }else
-  if( mode == MODE_TEAM_PROPAGATE ){
-    loop_mode_team_propagate();
-  }else
-  if( mode == MODE_PIECE_SELECT ){
-    loop_mode_piece_select();
-  }else
-  if( mode == MODE_PIECE_SELECT_FEEDBACK ){
-    loop_mode_piece_select_feedback();
-  }else
-  if( mode == MODE_BOARD ){
-    loop_mode_board();
-  }else
-  if( mode == MODE_BOARD_INFO ){
-    loop_mode_board_info();
-  }
-
-}
-
-void draw_animate_pulse( Color color, Timer timer, unsigned int duration, byte cycles ){
-  byte pulseMapped = map(timer.getRemaining(), 0, duration, 0, 255*cycles);
-  setColor( dim(color, sin8_C(pulseMapped)) );
-}
-void draw_animate_pulse_bitmap( byte* bitmap, Color color, Timer timer, unsigned int duration, byte cycles ){
-  byte pulseMapped = map(timer.getRemaining(), 0, duration, 0, 255*cycles);
-  draw_bitmap( bitmap, color, sin8_C(pulseMapped) );
-}
-void draw_animate_fade_in_function( void *draw(Color), Color color, Timer timer, unsigned int duration ){
-  byte pulseMapped = map(timer.getRemaining(), 0, duration, 0, 128);
-  draw( dim(color, sin8_C(pulseMapped)) );
-}
-void draw_animate_fade_out_function( void *draw(Color), Color color, Timer timer, unsigned int duration ){
-  byte pulseMapped = map(timer.getRemaining(), 0, duration, 128, 0);
-  draw( dim(color, sin8_C(pulseMapped)) );
-}
-void draw_bitmap( byte* bitmap, Color color, byte brightness ){
-  FOREACH_FACE( f ){
-    byte alpha = map( bitmap[f], 0, 255, 0, brightness );
-    setColorOnFace( dim(color,alpha), f );
-  }
-}
-
-/**
- * Given a result and a face, light the appropriate color on that face.
- * 
- * The idea is to be able to read the score directly from the game board. To
- * do that we draw the following:
- * If it's you vs you - draw white on both faces (otherwise you can win by
- * simply putting your pieces together).
- * Win: draw your color on both faces (+2 you)
- * Lose: draw your opponents color on both faces (+2 opponent)
- * Tie: draw your own color on your face (+1 you, +1 opponent)
- */
-void result_draw( byte result, byte other_team, byte this_team, byte face ){
-  if( other_team == this_team ){
-    setColorOnFace( dim( COLOR_SELF_WIN_LOSE, BRIGHTNESS_SELF ), face );
-  }else{
-    if( result == RESULT_FACE_WON ){
-      setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_WON ), face );
-    }else if( result == RESULT_FACE_LOST ){
-      setColorOnFace( dim( TEAM_COLORS[other_team], BRIGHTNESS_WON ), face );
-    }else if( result == RESULT_FACE_TIE ){
-      setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_WON ), face );
+    if( ! idleTimer.isExpired () ){
+        brightness = 2*sin8_C(map(idleTimer.getRemaining(),0,IDLE_TIMER_DURATION,0,128))-255;
     }else{
-      setColorOnFace( dim( TEAM_COLORS[team_index], BRIGHTNESS_LONELY ), face );
+        idleTimer.set(IDLE_TIMER_DURATION);
+        visibleChoice = 1+ ((visibleChoice + 1) % N_CHOICES)
     }
-  }
+     
+    if( mode == MODE_SHOW_CHOICE ){
+        if( ! clickTimer.isExpired() ){
+            brightness = map(clickTimer.getRemaining(),0,CLICK_TIMER_DURATION,0,255);
+            if( buttonPressed() ){
+                team = (team+1) % N_TEAMS;
+            }
+        }else{
+            mode = MODE_IDLE;
+            idleTimer.set(IDLE_TIMER_DURATION);
+        }
+    }
+
+    Color color = teamColors[team];
+    if( mode == MODE_CORE ){
+        setColor( WHITE );
+    }else{
+        setColor( dim(color,brightness) );
+    }
+    if( mode == MODE_SHOW_CHOICE || mode == MODE_CORE ){
+        visibleChoice = choice;
+    }
+    if( visibleChoice == ROCK ){
+        setColorOnFace(OFF, 0);
+    }
+    if( visibleChoice == PAPER ){
+        setColorOnFace( OFF, 0 );
+        setColorOnFace( OFF, 3 );
+    }
+    if( visibleChoice == SCISSORS ){
+        setColorOnFace( OFF, 0 );
+        setColorOnFace( OFF, 2 );
+        setColorOnFace( OFF, 4 );
+    }
 }
 
-/**
- * Given the value (ROCK/PAPER/SCISSORS) of this Blink and the value
- * of the opponent's blick -- tell me if I won, lost or tied.
- */
-byte result_lookup( byte my_value, byte your_value ){
-  if( my_value == your_value ){
-    return RESULT_FACE_TIE;
-  }
-  if( my_value == ROCK ){
-    if( your_value == PAPER ){
-      return RESULT_FACE_LOST;
-    }
-    if( your_value == SCISSOR ){
-      return RESULT_FACE_WON;
-    }
-  }
-  if( my_value == PAPER ){
-    if( your_value == SCISSOR ){
-      return RESULT_FACE_LOST;
-    }
-    if( your_value == ROCK ){
-      return RESULT_FACE_WON;
-    }
-  }
-  if( my_value == SCISSOR ){
-    if( your_value == ROCK ){
-      return RESULT_FACE_LOST;
-    }
-    if( your_value == PAPER ){
-      return RESULT_FACE_WON;
-    }
-  }
-}
